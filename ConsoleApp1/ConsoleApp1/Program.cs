fd497d2f4d9096003e190bccbd08de39279e7fb6
Михаил Титко <avalor@mail.ru>
2017-08-24 15:25:14 +0300

    Складыватель версия 1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Консольный складыватель");
+            Console.WriteLine("Введите цифру:");
+            string x = Console.ReadLine();
+            int a = Convert.ToInt32(x);
+            Console.WriteLine("Введите вторую цифру:");
+            string y = Console.ReadLine();
+            int b = Convert.ToInt32(y);
+            int c = a + b;
+            Console.WriteLine(c);
+
+            Console.ReadKey();
+        }
+
+    }
+}
